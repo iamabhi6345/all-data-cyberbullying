5486edb97bf5313b2a81bebe8be31ddde9b6b160
iamabhi6345 <iamabhi6345@gmail.com>
2024-07-08 21:08:19 +0530

    Initialized DVC

--- abhishek/utils/data_utils.py
+++ abhishek/utils/data_utils.py
@@ -18,6 +18,6 @@ def initialize_dvc() -> None:
     run_shell_command("dvc init")
     run_shell_command("dvc config core.analytics false")
     run_shell_command("dvc config core.autostage true")
-    run_shell_command("git add .dvc")
+    run_shell_command("git add .")
     run_shell_command("git commit -m 'Initialized DVC'")
 

ef39ea8184c60397ac1fcad1e2d9ae75788e5398
abhishek <iamabhi6345@gmail.com>
2024-07-08 20:24:45 +0530

    initialisation of dvc

--- abhishek/version_data.py
+++ abhishek/version_data.py
@@ -1,12 +1,13 @@
 from abhishek.config_schemas.config_schema import Config
 from abhishek.utils.config_utils import get_config
+from abhishek.utils.data_utils import initialize_dvc
+# , initialize_dvc_storage, make_new_data_version
 
 
 @get_config(config_path="../configs", config_name="config")
-def entrypoint(config: Config) -> None:
-    print(config)
+def version_data(config: Config) -> None:
+    initialize_dvc()
 
 
 if __name__ == "__main__":
-    entrypoint()  # type: ignore
-    
+    version_data()  
